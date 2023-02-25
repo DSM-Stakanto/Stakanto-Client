ac5dc2f6d6b1d2ae4f7c277ed9aeb058d1099df2
eastcopper <ldh7228@gmail.com>
2023-02-25 12:43:13 +0900

    Add request

---
+++ src/api/index.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+const request = axios.create({
+  baseURL: "http://54.150.212.81:8080",
+});
+request.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+request.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+export default request;
---
+++ src/api/token.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+const requestToken = axios.create({
+  baseURL: "http://54.150.212.81:8080",
+  headers: {
+    Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
+  },
+});
+requestToken.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+requestToken.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+export default requestToken;
