f80141cc56917df63c72bee22b5de03f085d5b9f
eastcopper <ldh7228@gmail.com>
2023-02-25 12:43:30 +0900

    Add Dist

---
+++ src/api/dist/index.ts
@@ -0,0 +1,7 @@
+export { getRankApi, type getRankResType } from "../main/getRank";
+export { loginApi, type loginResType } from "../main/auth/login";
+export { SignUpApi } from "../main/auth/signup";
+export { getUserApi, type getUserResType } from "../main/getUser";
+export { getLogApi, type getLogResType } from "../main/getLog";
+export { getMusicApi, type getMusicResType } from "../main/getMusic";
+export { postLogApi } from "../main/postLog";
