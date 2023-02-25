d6da84b2e64c449bbbd4d0b4af8d3cb4b0700421
eastcopper <ldh7228@gmail.com>
2023-02-25 16:47:37 +0900

    Add MainPage

---
+++ src/page/MainPage/index.tsx
@@ -0,0 +1,12 @@
+import MainPageSelect from "./Select";
+import MainPageProfile from "./Profile";
+
+const MainPage = () => {
+  return (
+    <>
+      <MainPageSelect />
+      <MainPageProfile />
+    </>
+  );
+};
+export default MainPage;
